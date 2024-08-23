c496bf5c5c4dcf43d80b792f1825bd2caf299cc4
vovakirdan <vova.kirdan@mail.ru>
2024-08-23 08:46:13 +0300

    First try

--- main.c
+++ main.c
@@ -1,12 +1,23 @@
 #include <stdint.h>
+#include <math.h>
+#include <stdio.h>
+
+uint64_t s(int n) {
+    return 0;
+}
+
+long double f(int n) {
+    return pow(log(n*sqrt(30)), 3)/(6*log(2)*log(3)*log(5));
+}
 
 uint64_t hamber(int n)
 {
-    return n;
-
+    return f(n) + log(log(n));
 }
 
 int main() {
-    printf("%i\n", hamber(1));
+    for (int i = 1; i < 120; i++) {
+        printf("%li\n", hamber(i));
+    }
     return 0;
 }
